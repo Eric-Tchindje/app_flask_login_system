7b65b09d83b0f6e463b9b0702a19e0d1bbe6b688
tchindje <tchindjeeric2@gmail.com>
2025-10-22 14:54:43 +0200

    add code to create db

--- app.py
+++ app.py
@@ -42,6 +42,9 @@ def create_app():
 # Gunicorn will use this
 app = create_app()
 
+# Create tables if not exist (safe on Render)
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
